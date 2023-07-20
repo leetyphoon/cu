80119f0558254a4d5f4e7fd96ec3e023fa04fe5d
KiYeon Lee <119033516+leetyphoon@users.noreply.github.com>
2023-07-20 11:32:53 +0900

    Update cu.h

--- cu.h
+++ cu.h
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 char *fmtstr(const char *format, ...);
+char **lstnew(size_t size);
+char *lstget(char **list, size_t idx);
+size_t lstset(char **list, size_t idx, char *val);
+size_t lstdel(char **list, size_t idx);
+void lstfree(char **list);
+void lstdump(char **list);
