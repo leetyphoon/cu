c8f7eec0a1f3073bf05be5d520977ee7302ade43
KiYeon Lee <119033516+leetyphoon@users.noreply.github.com>
2023-07-20 11:33:26 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,9 +1,22 @@
 #include "cu.h"
 
 int main(void) {
+/* fmtstr test
     char *str = fmtstr("Hello %s!! Num: %d", "World", 3);
     puts(str);
     free(str);
+*/
+/* string list test
+	char **list = lstnew(3);
+	lstset(list, 0, "Num: 0");
+	lstset(list, 1, "Num: 1");
+	lstset(list, 2, "Num: 2");
+	lstdel(list, 0);
+    lstdump(list);
 
+    printf("%s\n", lstget(list, 0));
+
+	lstfree(list);
+*/
     return EXIT_SUCCESS;
 }
